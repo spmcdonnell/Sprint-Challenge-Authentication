9d580f9575f8102dddeba8b8ee15ba72bbda8f13
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-09-20 12:25:37 -0400

    Complete register and login routes and add restriction to jokes route

--- api/server.js
+++ api/server.js
@@ -1,18 +1,45 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
+const dbConnection = require('../database/dbConfig.js');
 
 const server = express();
 
+const sessionConfig = {
+    name: 'chocochip',
+    secret: process.env.SESSION_SECRET || 'keep it secret, keep it safe',
+    cookie: {
+        maxAge: 1000 * 60 * 60,
+        secure: false,
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new KnexSessionStore({
+        knex: dbConnection,
+        tablename: 'knexsessions',
+        sidfieldname: 'sessionid',
+        createtable: true,
+        clearInterval: 1000 * 60 * 30
+    })
+};
+
 server.use(helmet());
 server.use(cors());
+server.use(session(sessionConfig));
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+
+server.get('/', (req, res) => {
+    res.send('API running');
+});
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,37 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const Users = require('../users/users-model.js');
 
 router.post('/register', (req, res) => {
-  // implement registration
+    let { username, password } = req.body;
+
+    const hash = bcrypt.hashSync(password, 10);
+
+    Users.add({ username, password: hash })
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                req.session.user = user;
+                res.status(200).json({ message: `Welcome ${user.username}!` });
+            } else {
+                res.status(401).json({ message: 'You cannot pass!' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
 });
 
 module.exports = router;
